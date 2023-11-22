54f7959ab9611d7117c109d1616952a2aa2ddea3
Sange Sherpa <sherpasange1215@gmail.com>
2023-11-22 12:11:56 +0545

    Initial Commit

---
+++ TodoRealm/TodoRealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoRealmApp.swift
+//  TodoRealm
+//
+//  Created by Sange Sherpa on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoRealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
